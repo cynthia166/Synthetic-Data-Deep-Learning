1ca2bb6e02ad90d20383c3df1893e93dbd8dceda
cynthia166 <cyn_n66@ciencias.unam.mx>
2024-04-17 12:27:36 -0400

    stats

--- evaluation/Untitled/resemblance/metric_stat.py
+++ evaluation/Untitled/resemblance/metric_stat.py
@@ -19,7 +19,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 import seaborn as sns
+from evaluation.privacy.metric_privacy import*
+
 class JensenShannonDistance:
+    
     """Evaluate the average Jensen-Shannon distance (metric) between two probability arrays."""
     
     def __init__(self, normalize: bool = False) -> None:
@@ -219,5 +222,228 @@ def corr_plot(total_features_train,type_im ):
     # Mostrar el gráfico
     plt.savefig(ruta_correl)
     plt.show()
+
+
+import pickle
+from tqdm import tqdm
+import numpy as np
+
+# Define a function to calculate statistics on the visits without considering labels
+def generate_statistics(ehr_datasets):
+    stats = {}
+
+    for label, dataset in ehr_datasets:
+        aggregate_stats = {}
+        record_lens = [len(p['visits'][0]) for p in dataset]  # List of record lengths
+        visit_lens = [len(v) for p in dataset for v in p['visits']]  # List of visit lengths
+
+        # Calculating aggregates
+        avg_record_len = np.mean(record_lens)
+        std_record_len = np.std(record_lens)
+        avg_visit_len = np.mean(visit_lens)
+        std_visit_len = np.std(visit_lens)
+
+        # Storing results
+        aggregate_stats["Record Length Mean"] = avg_record_len
+        aggregate_stats["Record Length Standard Deviation"] = std_record_len
+        aggregate_stats["Visit Length Mean"] = avg_visit_len
+        aggregate_stats["Visit Length Standard Deviation"] = std_visit_len
+
+        stats[label] = aggregate_stats
+
+    return stats
+
+
+
+def plot_age(df,col,name):
+    # Set the background style
+    from scipy.stats import gaussian_kde
+    plt.style.use("seaborn-white")
+
+    # Define two shades of blue
+    dark_blue = "#00008B"   # A dark blue
+    light_blue = "#ADD8E6"  # A light blue
+
+    # Create a figure and axes
+    fig, ax = plt.subplots(figsize=(7, 4))
+
+    # Calculate the kernel density estimate for 'Age_max' for each gender
+   
+    age_max_female = df[col]
+   
+    kde_age_max_female = gaussian_kde(age_max_female)
+
+    # Generate x-values for the kernel density estimate
+    x_age_max = np.linspace(age_max_female.min(), age_max_female.max(), 100)
+
+    ax.plot(x_age_max, kde_age_max_female(x_age_max), color=light_blue, label='Gaussian KDE')
+
+    # Set the labels and title for the 'Age_max' plot
+    ax.set_xlabel(str(col))
+    ax.set_ylabel('Density')
+    ax.set_title('Kernel Density Plot - ' + str(col))
+    ax.set_ylim(bottom=0)  # Set the y-axis limit to start at zero
+    plt.savefig('results_SD/img/'+col+'_'+name+'_kernelplot.svg')
+    # Show the plot
+    plt.show()
+
+
+def plot_procedures_diag_drugs(col_prod,train_ehr_dataset,type_procedur,name):
+    df = train_ehr_dataset[col_prod+["id_patient","visit_rank"]]
+    if type_procedur =="procedures":
+        result_subject = df.groupby("id_patient").size().reset_index(name='Count')
+        result_admission = df.groupby("visit_rank").size().reset_index(name='Count')
+        label_xl = 'Count of ICD-9 codes'
+    elif type_procedur=="diagnosis":
+        result_subject = df.groupby("id_patient").size().reset_index(name='Count')
+        result_admission = df.groupby("visit_rank").size().reset_index(name='Count')
+        #result_subject_1 = result_subject[result_subject["Count"]<100]
+        #result_admission_1 = result_admission[result_admission["Count"]<100]
+        label_xl = 'Count of ICD-9 codes'
+    elif type_procedur=="drugs":          
+        result_subject = df.groupby("id_patient").size().reset_index(name='Count')
+        result_admission = df.groupby("visit_rank").size().reset_index(name='Count')
+        #result_subject_1 = result_subject[result_subject["Count"]<300]
+        #result_admission_1 = result_admission[result_admission["Count"]<150]
+        label_xl = 'Count of drugs'
+    else:
+        print( "Type of procedure not found"   ) 
+    # Create a figure with two matplotlib.Axes objects
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
+    # Assigning a graph to each ax
+    sns.histplot(data=result_subject, x="Count", ax=ax1, color='darkblue',bins = 50)
+    sns.histplot(data=result_admission, x="Count", ax=ax2, color='lightblue',bins = 30)
+    # Set x-axis and y-axis labels for each subplot
+    ax1.set(xlabel='Count of ICD-9 codes per patient' + name, ylabel='Frequency')
+    ax2.set(xlabel=label_xl+' per admission', ylabel='Frequency')
+    ax1.text(-0.1, -0.2, '(a)', transform=ax1.transAxes, size=10, )
+    ax2.text(-0.1, -0.2, '(b)', transform=ax2.transAxes, size=10, )
+    fig.suptitle('Count of ICD-9 codes '+type_procedur, fontsize=14)  # Increase the font size of the title
+    # Show the plot
+    plt.savefig('results_SD/hist/'+type_procedur+'_'+name+'_kernelplot.svg')
+    plt.show()
+
+# Example datasets
+
+
+# Save the statistics to a file
+
+
+
+path_o = "train_sp/"    
+attributes_path_train= "non_prepo/DATASET_NAME_non_preprotrain_data_attributes.pkl"
+features_path_train = "non_prepo/DATASET_NAME_non_preprotrain_data_features.pkl"
+features_path_valid = "non_prepo/DATASET_NAME_non_preprovalid_data_features.pkl"
+attributes_path_valid = "non_prepo/DATASET_NAME_non_preprovalid_data_attributes.pkl"
+synthetic_path_attributes = 'non_prepo/DATASET_NAME_non_prepronon_prepo_synthetic_attributes_10.pkl'
+synthetic_path_features = 'non_prepo/DATASET_NAME_non_prepronon_prepo_synthetic_features_10.pkl'
+
+# se lee los archivos y se obtiene del la longitude de valid
+total_features_synthethic, total_fetura_valid,total_features_train,attributes =  get_valid_train_synthetic (path_o, attributes_path_train, features_path_train, features_path_valid, attributes_path_valid,synthetic_path_attributes,synthetic_path_features)
+# se transforma de numpy array 3 dimensiones a dataframe
+total_features_synthethic,total_fetura_valid,total_features_train = obtener_dataframe_inicial_denumpyarrray(total_features_synthethic, total_fetura_valid,total_features_train )
+
+# esta es para agregar la columnas
+dataset_name = 'DATASET_NAME_non_prepo'
+file_name = "train_sp/non_prepo/DATASET_NAME_non_prepo_non_preprocess.pkl"
+aux = load_data(file_name)
+#aux = load_data(DARTA_INTERM_intput + dataset_name + '_non_preprocess.pkl')
+con_cols = list(aux[0].columns)
+static = pd.read_csv("train_sp/non_prepo/static_data_non_preprocess.csv")
+# Suponiendo que 'total_features_synthethic' es tu DataFrame
+if 'Unnamed' in static.columns:
+    static = static.drop(columns=['Unnamed'])
+cat = list(static.columns[2:]) +["visit_rank","id_patient"  ]
+del aux
+total_cols =  con_cols+cat 
+cat1 = list(static.columns[2:]) +["visit_rank","id_patient","max_consultas"     ]
+total_cols1 =  con_cols+cat1 
+
+test_ehr_dataset,train_ehr_dataset,synthetic_ehr_dataset = preprocess_data(total_cols,total_features_synthethic,total_cols1,total_fetura_valid,total_features_train)
+
+
+#obtener coluans que contenga diagnosis,procedures,drugs
+columnas_test_ehr_dataset = get_cols_diag_proc_drug(train_ehr_dataset)
+
+#obtener n mas frequent codes
+top_300_codes = obtain_most_freuent(train_ehr_dataset,columnas_test_ehr_dataset,100)
+
+#obtener un syntethic datafram que considere el percentil y si es mayor a eso se considera 1 si no 0, si es false no se le agrega la columnas id_patient
+synthetic_ehr = change_tosyn_stickers_temporal(synthetic_ehr_dataset,columnas_test_ehr_dataset,True)
+
+dataset_train = obtain_dataset(train_ehr_dataset,columnas_test_ehr_dataset)
+dataset_test = obtain_dataset(test_ehr_dataset,columnas_test_ehr_dataset)
+
+dataset_syn = obtain_dataset(synthetic_ehr,columnas_test_ehr_dataset)
+
+##obtener static lenght o stay
+ehr_datasets = [
+    ('Train', dataset_train),
+    ('Test', dataset_test),
+    ('Synthetic', dataset_syn),
+    # ... other datasets
+]
+
+# continous_plot
+statistics = generate_statistics(ehr_datasets)
+plot_age(test_ehr_dataset,"Age_max","test")
+plot_age(synthetic_ehr,"Age_max","synthetic")
+plot_age(train_ehr_dataset,"Age_max","synthetic")
+#plot histograms
+keywords = ['diagnosis', 'procedures', 'drugs']
+for i in keywords:
+    col_prod = [col for col in train_ehr_dataset.columns if any(palabra in col for palabra in [i])]
+    name = "Train"
+    type_procedur = i
+
+    plot_procedures_diag_drugs(col_prod,train_ehr_dataset,type_procedur,name)
+
+    col_prod = [col for col in test_ehr_dataset.columns if any(palabra in col for palabra in [i])]
+    name = "Test"
+
+
+    plot_procedures_diag_drugs(col_prod,test_ehr_dataset,type_procedur,name)
+    
+    col_prod = [col for col in synthetic_ehr.columns if any(palabra in col for palabra in [i])]
+    name = "Synthetic"
+
+
+    plot_procedures_diag_drugs(col_prod,synthetic_ehr_dataset,type_procedur,name)
+    
+
+    
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+import pandas as pd
+
+def plot_tsne(name,X_gt_df: pd.DataFrame, X_syn_df: pd.DataFrame) -> None:
+    # Create the figure and axis for plotting
+    fig, ax = plt.subplots(1, 1, figsize=(12, 10))
+
+    # Compute t-SNE for ground truth data
+    tsne_gt = TSNE(n_components=2, random_state=0, learning_rate="auto", init="pca")
+    proj_gt = pd.DataFrame(tsne_gt.fit_transform(X_gt_df.values), columns=['x', 'y'])
+
+    # Compute t-SNE for synthetic data
+    tsne_syn = TSNE(n_components=2, random_state=0, learning_rate="auto", init="pca")
+    proj_syn = pd.DataFrame(tsne_syn.fit_transform(X_syn_df.values), columns=['x', 'y'])
+
+    # Scatter plot for ground truth and synthetic data
+    ax.scatter(x=proj_gt['x'], y=proj_gt['y'], s=10, label="Real data")
+    ax.scatter(x=proj_syn['x'], y=proj_syn['y'], s=10, label="Synthetic data")
+
+    # Set legend and labels
+    ax.legend(loc="upper left")
+    ax.set_title("t-SNE plot")
+    ax.set_xlabel("t-SNE axis 1")
+    ax.set_ylabel("t-SNE axis 2")
+    plt.savefig('results_SD/hist/tsne'+'_'+name+'_kernelplot.svg')
+    # Display the plot
+    plt.show()
+
+# Example usage with two dataframes, `X_gt_df` and `X_syn_df`
+test_ehr_dataset = test_ehr_dataset[:synthetic_ehr_dataset.shape[0]]
+plot_tsne("Doop",test_ehr_dataset, synthetic_ehr_dataset)
+
 
 
